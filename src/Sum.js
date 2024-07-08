c4b6840434c444226edaefb24acfc135887a728e
Jayeeta22 <jayeeta97.riya@gmail>
2024-07-08 21:02:44 +0530

    created 1st testcase

---
+++ src/Sum.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function Sum(a,b) {
+  return a+b
+}
+
---
+++ src/Sum.test.js
@@ -0,0 +1,4 @@
+import Sum from "./Sum";
+test("testing sum function",()=>{
+    expect(Sum(10,10)).toBe(20)
+})
