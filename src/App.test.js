6374fd871bad91b7e3b18ce8d09604fd57026885
Jayeeta22 <jayeeta97.riya@gmail>
2024-07-09 10:29:06 +0530

    input field possible text cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import {act} from 'react-dom/test-utils';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Jayeeta/i);
   expect(linkElement).toBeInTheDocument();
+
+  const input=screen.getByRole("textbox")
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveAttribute("name", "username")
+  expect(input).toHaveAttribute("id", "userId")
+
+  const placeholder=screen.getByPlaceholderText("type here")
+  expect(placeholder).toBeInTheDocument();
 });
+ 
