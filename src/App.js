7bbdde63af2657a4040705ad64f144596f8af9e9
Jayeeta22 <jayeeta97.riya@gmail>
2024-10-08 13:36:11 +0530

    check11

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data,setData]=useState("")
   return (
     <div className="App">
-     <div>hello Jayeeta11</div>
+     <div>hello Jayeeta111</div>
      <input placeholder='type here' name='username' id='userId' value={data} onChange={(e)=>setData(e.target.value)} />
     </div>
   );
